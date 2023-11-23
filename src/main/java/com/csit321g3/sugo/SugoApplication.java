0a5dc370f64e59af930c3bb4bb7151d7815b3c2f
YAGOO (not real) <93402922+rosnuker@users.noreply.github.com>
2023-11-24 00:46:52 +0800

    Initialize Sugo Backend

---
+++ src/main/java/com/csit321g3/sugo/SugoApplication.java
@@ -0,0 +1,13 @@
+package com.csit321g3.sugo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugoApplication.class, args);
+	}
+
+}
